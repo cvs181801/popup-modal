abf0623535d4aef8416c69a8d02e06c4db892d88
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-05-12 15:03:07 -0700

    add to ideas

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 document.querySelector('.start-button').addEventListener("click", function() {
     document.querySelector('.bg-modal').style.display = "flex";
 });
-document.querySelector('.close').addEventListener('click', function(){
+document.querySelector('.close').addEventListener('click', function() {
     document.querySelector('.bg-modal').style.display = "none";
 });
+
+//document.querySelector('.submit-button').addEventListener('click', function() {
+//    document.createElement('a')
+//}) https://seattlevideomarketing.com/contact-us/
